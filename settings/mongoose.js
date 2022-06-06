d5fd2440c7023b629731793f7a172e88c1f61b12
nico <nicolas.escudero00@gmail.com>
2022-06-06 13:23:21 -0300

    heroku

--- settings/mongoose.js
+++ settings/mongoose.js
@@ -1,9 +1,20 @@
 const mongoose=require('mongoose');
-const connect=async()=>
-await mongoose.connect(process.env.DB_URI,{
+
+const {NODE_ENV,DB_URI_DEVELOPMENT,DB_URI_CONNECTION}=process.env;
+let uri='';
+
+if(NODE_ENV === 'production')uri=DB_URI_CONNECTION;
+else uri=DB_URI_DEVELOPMENT;
+
+
+const options={
     useNewUrlParser: true,
     useUnifiedTopology: true,
-})
+    serverSelectionTimeoutMS: 5000,
+}
+
+const connect=async()=>
+await mongoose.connect(uri,options)
 .then(()=>console.log(`Connected to mongodb`))
 .catch(error=>console.log(error));
 
