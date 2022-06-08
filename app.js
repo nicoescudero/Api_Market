d2c6e48c730fdb4ced7940a2c26e9b5a326957d0
nico <nicolas.escudero00@gmail.com>
2022-06-08 19:18:36 -0300

    remove morgan

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express=require('express');
 const app=express();
-const morgan=require('morgan');
 const path=require('path');
 const cors=require('cors');
 require('dotenv').config();
@@ -10,7 +9,6 @@ app.use(cors());
 app.use(express.static(path.join(__dirname,'public')));
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
-app.use(morgan('dev'));
 //routes
 app.use('/',require('./routes/index.routes'));
 
