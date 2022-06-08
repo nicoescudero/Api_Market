952f7b0f0941551a884a8390414bbce3eb4e2836
nico <nicolas.escudero00@gmail.com>
2022-06-08 17:54:31 -0300

    change white list of cors

--- app.js
+++ app.js
@@ -8,13 +8,12 @@ require('./settings/mongoose');
 //settings
 app.set('port',process.env.PORT || 3000);
 //middlewares
-app.use(cors({origin:['https://nicoescudero.github.io/React-Market-App']}))
+app.use(cors({origin:'https://market-app-light.herokuapp.com'}));
 app.use(express.static(path.join(__dirname,'public')));
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use(morgan('dev'));
 //routes
 app.use('/',require('./routes/index.routes'));
-
 
 app.listen(app.get('port'),()=>console.log(`PORT: ${app.get('port')}`));
